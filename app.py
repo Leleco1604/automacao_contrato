bbab3a697e630253cfaad433c0c9b4b688d84766
Leonardo Barbosa <87045040+Leleco1604@users.noreply.github.com>
2024-05-06 17:04:44 -0300

    changes

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-# - Passar as informações de planilha para um arquivo word 
-# - Salvar aquele arquivo word em uma pasta especifica (contratos)
-# - Repetir para todas as linhas da planilha
-
 from openpyxl import load_workbook
 from docx import Document
 from datetime import datetime
 
+# - Passar as informações de planilha para um arquivo word 
 
 planilha_fornecedores = load_workbook('./fornecedores.xlsx')
 pagina_fornecedores = planilha_fornecedores['Sheet1']
+# - Repetir para todas as linhas da planilha
 
 for linha in pagina_fornecedores.iter_rows(min_row=2, values_only= True):
     nome_empresa, endereco, cidade, estado, cep, telefone, email, setor = linha #unpacking
@@ -45,3 +43,8 @@ for linha in pagina_fornecedores.iter_rows(min_row=2, values_only= True):
 
     [Recife], {datetime.now().strftime('%d/%m/%Y')}
     """
+
+    arquivo_word.add_paragraph(texto_contrato)
+# - Salvar aquele arquivo word em uma pasta especifica (contratos)
+
+    arquivo_word.save(f'./contratos/contrato_{nome_empresa}.docx')
